f2ffd8a154239e97f79f5abe1dc465dd1dd05c1f
churmeev <churmeev@yandex.ru>
2021-02-18 04:35:12 +0300

    add interface for parser

---
+++ src/main/java/com/payment/parser/PaymentParser.java
@@ -0,0 +1,21 @@
+package com.payment.parser;
+
+import com.payment.parser.model.PaymentRecord;
+
+import java.util.List;
+
+/**
+ * Interface for parse payment records
+ *
+ * @author Churmeev Dmitriy
+ * @since 16.02.2021
+ */
+public interface PaymentParser {
+
+    /**
+     * Parse file and return List of Payment Record from file
+     * @param file file name with Payment Record
+     * @return List Payment Record {@link PaymentRecord}
+     */
+    List<PaymentRecord> parse(String file);
+}
